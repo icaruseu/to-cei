90137691be93e83066b0062301de43fb5687c39c
Daniel Jeller <djeller@nettek.at>
2021-12-27 12:31:00 +0100

    Improve charter id model and add missing tests

--- model/charter.py
+++ model/charter.py
@@ -1,4 +1,5 @@
-from typing import List
+from typing import List, Optional
+from urllib.parse import quote
 
 from lxml import etree
 from lxml.builder import ElementMaker
@@ -9,17 +10,25 @@ CEI = ElementMaker(namespace=CEI_NS, nsmap={None: CEI_NS})
 
 class Charter:
     _abstract_bibls: List[str]
-    _id: str
+    _id_norm: str
+    _id_old: Optional[str]
+    _id_text: str
     _transcription_bibls: List[str]
 
     def __init__(
         self,
-        id: str,
+        id_text: str,
         abstract_bibls: str | List[str] = [],
+        id_norm: Optional[str] = None,
+        id_old: Optional[str] = None,
         transcription_bibls: str | List[str] = [],
     ):
+        if not id_text:
+            raise Exception("id_text is not allowed to be empty")
         self.abstract_bibls = abstract_bibls
-        self.id = id
+        self.id_norm = id_norm if id_norm else id_text
+        self.id_old = id_old
+        self.id_text = id_text
         self.transcription_bibls = transcription_bibls
 
     # --------------------------------------------------------------------#
@@ -35,12 +44,28 @@ class Charter:
         self._abstract_bibls = value if isinstance(value, List) else [value]
 
     @property
-    def id(self):
-        return self._id
+    def id_norm(self):
+        return self._id_norm
 
-    @id.setter
-    def id(self, value: str):
-        self._id = value
+    @id_norm.setter
+    def id_norm(self, value: str):
+        self._id_norm = quote(value)
+
+    @property
+    def id_old(self):
+        return self._id_old
+
+    @id_old.setter
+    def id_old(self, value: Optional[str]):
+        self._id_old = value
+
+    @property
+    def id_text(self):
+        return self._id_text
+
+    @id_text.setter
+    def id_text(self, value: str):
+        self._id_text = value
 
     @property
     def transcription_bibls(self):
@@ -51,44 +76,57 @@ class Charter:
         self._transcription_bibls = value if isinstance(value, List) else [value]
 
     # --------------------------------------------------------------------#
-    #                          Private methods                           #
+    #                        Private CEI creators                        #
     # --------------------------------------------------------------------#
-
-    def _build(self) -> etree.Element:
-        return self._create_cei_text()
 
     def _create_cei_back(self) -> etree.Element:
         return CEI.back()
 
     def _create_cei_body(self) -> etree.Element:
-        return CEI.body(CEI.idno(self._id, id=self._id))
+        children = [self._create_cei_idno]
+        return CEI.body(*children)
 
     def _create_cei_front(self) -> etree.Element:
-        childs = []
+        children = []
+        source_desc = self._create_cei_source_desc()
+        if len(source_desc):
+            children.append(source_desc)
+        return CEI.front(*children)
+
+    def _create_cei_idno(self) -> etree.Element:
+        attributes = {"id": self.id_norm}
+        if self.id_old:
+            attributes["old"] = self.id_old
+        return CEI.idno(self.id_text, **attributes)
+
+    def _create_cei_source_desc(self) -> etree.Element:
+        children = []
         if self.abstract_bibls:
-            childs.append(
+            children.append(
                 CEI.sourceDescRegest(*[CEI.bibl(bibl) for bibl in self.abstract_bibls])
             )
         if self.transcription_bibls:
-            childs.append(
+            children.append(
                 CEI.sourceDescVolltext(
                     *[CEI.bibl(bibl) for bibl in self.transcription_bibls]
                 )
             )
-        if childs:
-            source_desc = CEI.sourceDesc(*childs)
-            return CEI.front(source_desc)
-        else:
-            return CEI.front()
+        return CEI.sourceDesc(*children)
 
     def _create_cei_text(self) -> etree.Element:
         return CEI.text(
             self._create_cei_front(),
             self._create_cei_body(),
             self._create_cei_back(),
-            id=self._id,
             type="charter",
         )
+
+    # --------------------------------------------------------------------#
+    #                          Private methods                           #
+    # --------------------------------------------------------------------#
+
+    def _build(self) -> etree.Element:
+        return self._create_cei_text()
 
     # --------------------------------------------------------------------#
     #                           Public methods                           #
--- test/model/test_charter.py
+++ test/model/test_charter.py
@@ -1,22 +1,14 @@
 from typing import List
 
+import pytest
 from lxml import etree
 
 from model.charter import CEI_NS, Charter
 from model.validator import Validator
 
 
-def test_is_valid_charter():
-    charter = Charter(
-        id="1",
-        abstract_bibls=["HAUSWIRTH, Schotten (=FRA II/18, 1859) S. 123, Nr. 103"],
-        transcription_bibls="HAUSWIRTH, Schotten (=FRA II/18, 1859) S. 123-124",
-    )
-    Validator().validate_cei(charter.to_xml())
-
-
-def test_is_correct_base_structure():
-    charter_xml = Charter(id="1").to_xml()
+def test_has_correct_base_structure():
+    charter_xml = Charter(id_text="1").to_xml()
     direct_children = charter_xml.xpath(
         "/cei:text/child::*", namespaces={"cei": CEI_NS}
     )
@@ -25,3 +17,116 @@ def test_is_correct_base_structure():
     assert etree.QName(direct_children[0].tag).localname == "front"
     assert etree.QName(direct_children[1].tag).localname == "body"
     assert etree.QName(direct_children[2].tag).localname == "back"
+
+
+def test_has_correct_abstract_bibl():
+    bibl_text = "Bibl a"
+    charter = Charter(
+        id_text="1",
+        abstract_bibls=bibl_text,
+    ).to_xml()
+    bibls = charter.xpath(
+        "/cei:text/cei:front/cei:sourceDesc/cei:sourceDescRegest/*",
+        namespaces={"cei": CEI_NS},
+    )
+    assert isinstance(bibls, List)
+    assert len(bibls) == 1
+    assert bibls[0].text == bibl_text
+
+
+def test_has_correct_abstract_bibls():
+    bibl_texts = ["Bibl a", "Bibl b"]
+    charter = Charter(
+        id_text="1",
+        abstract_bibls=bibl_texts,
+    ).to_xml()
+    bibls = charter.xpath(
+        "/cei:text/cei:front/cei:sourceDesc/cei:sourceDescRegest/*",
+        namespaces={"cei": CEI_NS},
+    )
+    assert isinstance(bibls, List)
+    assert len(bibls) == 2
+    assert bibls[0].text == bibl_texts[0]
+    assert bibls[1].text == bibl_texts[1]
+
+
+def test_has_correct_id():
+    id = "~!1307 II 22|23.Ⅱ"
+    id_norm = "~%211307%20II%2022%7C23.%E2%85%A1"
+    charter = Charter(id_text=id)
+    assert charter.id_text == id
+    assert charter.id_norm == id_norm
+    charter_xml = charter.to_xml()
+    idno = charter_xml.xpath("/cei:text/cei:body/cei:idno", namespaces={"cei": CEI_NS})
+    assert isinstance(idno, List)
+    assert len(idno) == 1
+    assert idno[0].get("id") == id_norm
+    assert idno[0].text == id
+
+
+def test_has_correct_id_norm():
+    id_norm = "1307_%E2%85%A1_22"
+    charter = Charter(id_text="1307 Ⅱ 22", id_norm="1307_Ⅱ_22")
+    assert charter.id_norm == id_norm
+    charter_xml = charter.to_xml()
+    idno = charter_xml.xpath("/cei:text/cei:body/cei:idno", namespaces={"cei": CEI_NS})
+    assert idno[0].get("id") == id_norm
+
+
+def test_has_correct_id_old():
+    id_old = "123456 α"
+    charter = Charter(id_text="1307 II 22", id_old=id_old)
+    assert charter.id_old == id_old
+    charter_xml = charter.to_xml()
+    idno = charter_xml.xpath("/cei:text/cei:body/cei:idno", namespaces={"cei": CEI_NS})
+    assert idno[0].get("old") == id_old
+
+
+def test_has_correct_transcription_bibl():
+    bibl_text = "Bibl a"
+    charter = Charter(
+        id_text="1",
+        transcription_bibls=bibl_text,
+    ).to_xml()
+    bibls = charter.xpath(
+        "/cei:text/cei:front/cei:sourceDesc/cei:sourceDescVolltext/*",
+        namespaces={"cei": CEI_NS},
+    )
+    assert isinstance(bibls, List)
+    assert len(bibls) == 1
+    assert bibls[0].text == bibl_text
+
+
+def test_has_correct_transcription_bibls():
+    bibl_texts = ["Bibl a", "Bibl b"]
+    charter = Charter(
+        id_text="1",
+        transcription_bibls=bibl_texts,
+    ).to_xml()
+    bibls = charter.xpath(
+        "/cei:text/cei:front/cei:sourceDesc/cei:sourceDescVolltext/*",
+        namespaces={"cei": CEI_NS},
+    )
+    assert isinstance(bibls, List)
+    assert len(bibls) == 2
+    assert bibls[0].text == bibl_texts[0]
+    assert bibls[1].text == bibl_texts[1]
+
+
+def test_has_correct_type():
+    charter = Charter(id_text="1").to_xml()
+    assert charter.get("type") == "charter"
+
+
+def test_is_valid_charter():
+    charter = Charter(
+        id_text="1307 II 22",
+        abstract_bibls=["HAUSWIRTH, Schotten (=FRA II/18, 1859) S. 123, Nr. 103"],
+        transcription_bibls="HAUSWIRTH, Schotten (=FRA II/18, 1859) S. 123-124",
+    )
+    Validator().validate_cei(charter.to_xml())
+
+
+def test_raises_exception_for_missing_id():
+    with pytest.raises(Exception):
+        Charter("")
